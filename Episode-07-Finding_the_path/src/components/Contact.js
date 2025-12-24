72411d89bfcb5e6499f278f3f409d5232da24270
JATINSINGHRAWAT <jatinsinghrawa759@gmail.com>
2025-12-25 03:16:19 +0530

    Episode -07 about routing

---
+++ Episode-07-Finding_the_path/src/components/Contact.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return(
+        <div>
+            <h1>Contact</h1>
+            <h2>This is Contact Page</h2>
+        </div>
+    ) 
+}
+
+export default About;
---
+++ Episode-07-Finding_the_path/src/components/Header.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+
+const Header = () =>{
+    console.log("header render");
+    // let btnName = "Login";
+    const [btnNameReact,setBtnNameReact] = useState("Login");
+    return(
+        <div className='header-container'>
+            <div className='logo'>
+                <img src={LOGO_URL}/>
+            </div>
+            <div className='nav-items'>
+                <ul className='nav-links'>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li>Cart</li>                
+                    {/* <button className="login" onClick={()=>{btnName="Logout"
+                     console.log(btnName)}}>
+                        {btnName}
+                    </button>  */}
+                    <button className="login" onClick={()=>btnNameReact==="Login"?setBtnNameReact("Logout"):setBtnNameReact("Login")}>
+                        {btnNameReact}
+                    </button>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
