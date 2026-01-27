1d8d70d8ed8d9873b9aaf77076f65d239ff303a7
JATIN SINGH RAWAT <140249217+JatinSinghRawat@users.noreply.github.com>
2026-01-27 12:21:03 +0530

    Revert "Episode-11"

--- Episode-11-Data_is_the_new_oil/src/utils/UserContext.js
+++
@@ -1,9 +0,0 @@
-//this is accessible anywhere in the app
-
-import { createContext } from "react";
-
-const UserContext = createContext({
-    loggedInUser:"Default User",
-});
-
-export default UserContext;
